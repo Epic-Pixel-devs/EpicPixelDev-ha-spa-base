43f1d3714e799c963786cada5388a7a542eb4db1
Souzatitan <asouzanderson@gmail.com>
2024-03-19 19:58:05 -0300

    finalisar tarefa'

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Alterado para Test
+          Alterado para Inicio
         </p>
         <a
           className="App-link"
